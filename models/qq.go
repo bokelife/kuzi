9e907666f42960a9e67d4b4e74e3d2bd40efb518
bokelife <johnnycai@foxmail.com>
2020-11-28 13:37:10 +0800

    Update qq.go

--- models/qq.go
+++ models/qq.go
@@ -10,6 +10,10 @@ type Qq struct {
 	Phone   uint64 `json:"phone"`
 }
 
+func (Qq) TableName() string {
+	return "qq_phone_8e"
+  }
+
 func FindByQq(qq string) (QqList []*Qq, err error){
 	if err = dao.DB.Where("qq=?", qq).Find(&QqList).Error; err != nil{
 		return nil, err
